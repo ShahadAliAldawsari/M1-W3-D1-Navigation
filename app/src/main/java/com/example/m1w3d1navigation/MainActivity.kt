3aacefa7d645d59405f3a65247f31bebfef0947b
Shahad Ali Aldawsari <shahodah.ali@gmail.com>
2025-03-16 21:55:56 +0300

    Added a Dynamic TopAppBar Implementation

--- app/src/main/java/com/example/m1w3d1navigation/MainActivity.kt
+++ app/src/main/java/com/example/m1w3d1navigation/MainActivity.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.m1w3d1navigation
 
 import android.os.Bundle
@@ -7,14 +9,27 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -42,7 +57,7 @@ class MainActivity : ComponentActivity() {
 
                     composable<ScreenB>{
                         val args = it.toRoute<ScreenB>()
-                        Screen_B(args.value)
+                        Screen_B(args.title,navCtrl)
                     }
                 }
             }
@@ -55,13 +70,15 @@ class MainActivity : ComponentActivity() {
 @Serializable
 object ScreenA
 
+
 @Serializable
 data class ScreenB(
-    val value: Int?
+    val title: String
 )
 
 @Composable
 fun Screen_A(navCtrl: NavHostController) {
+    Header(screenTitle = "Screen A")
     Column(
         modifier = Modifier
             .fillMaxSize(),
@@ -76,13 +93,14 @@ fun Screen_A(navCtrl: NavHostController) {
             fontSize = 23.sp,
         )
 
+        Spacer(Modifier.height(32.dp))
+
         Button(
-            onClick = { navCtrl.navigate(ScreenB(value = 1))
+            onClick = { navCtrl.navigate(ScreenB(title = "Screen B"))
         }
         ) {
-//
             Text(
-                text = "Click to go!",
+                text = "Click to visit B!",
                 modifier = Modifier,
                 textAlign = TextAlign.Start,
                 fontSize = 23.sp,
@@ -95,8 +113,8 @@ fun Screen_A(navCtrl: NavHostController) {
 }
 
 @Composable
-fun Screen_B(value: Int?){
-
+fun Screen_B(screenTitle: String, navCtrl: NavHostController){
+    Header(screenTitle=screenTitle)
     Column(
         modifier = Modifier
             .fillMaxSize(),
@@ -111,6 +129,62 @@ fun Screen_B(value: Int?){
             fontSize = 23.sp,
         )
 
-    }
+    Spacer(Modifier.height(32.dp))
 
+        Button(
+            onClick={navCtrl.navigate(ScreenA)}
+        ){
+            Text(
+                text = "Click to visit A!",
+                fontSize = 23.sp,
+                color = Color.White,
+            )
+        }
+    }
 }
+
+@Composable
+fun Header(modifier: Modifier = Modifier,
+           screenTitle: String
+           ){
+    TopAppBar(
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
+        title = {
+            Text(
+                text = screenTitle,
+                style = MaterialTheme.typography.titleLarge,
+            )
+        },
+        navigationIcon = {
+            Icon(
+                imageVector = Icons.Filled.Menu,
+                contentDescription = "nav icon",
+                modifier = Modifier
+                    .width(30.dp)
+                    .height(30.dp),
+
+            )
+        },
+        actions = {
+            Icon(
+                imageVector = Icons.Filled.AccountCircle,
+                contentDescription = "account icon",
+                modifier = Modifier
+                    .width(30.dp)
+                    .height(30.dp)
+            )
+
+        },
+    )
+}
+
+
+
+
+
+
+
+
+
