e495c1ed16cca7040faa76fdfee2beb30203f5c7
Shahad Ali Aldawsari <shahodah.ali@gmail.com>
2025-03-16 13:46:04 +0300

    fMade a new project

---
+++ app/src/main/java/com/example/m1w3d1navigation/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.m1w3d1navigation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.m1w3d1navigation.ui.theme.M1W3D1NavigationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            M1W3D1NavigationTheme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(
+                                    text = "Screen A"
+                                )
+                            }
+                        )
+                    }
+                    
+                ) { innerPadding ->
+                    sumfunction(innerPadding)
+                }
+            }
+        }
+    }
+}
